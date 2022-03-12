e34deecd940c269ba1a8625a2d5af9b84578b4db
NYNEX SERVER 5 <65372786+CodeSalvageON@users.noreply.github.com>
2022-03-12 19:48:05 +0000

    Finished all widgets except for calculator

--- script.js
+++ script.js
@@ -53,6 +53,8 @@ const reset_all = document.getElementById("reset-all");
 
 const imperium_options = document.getElementById("imperium-options");
 const af_options = document.getElementById("af-options");
+const proxy_options = document.getElementById("proxy-options");
+const unblocker_options = document.getElementById("unblocker-options");
 
 let selected_widget = 0;
 let widget_1_widget = "";
@@ -70,6 +72,20 @@ widget1_btn.onclick = function () {
   else if (widget_1_widget === "finder") {
     widget_btns.style.display = "none";
     af_options.style.display = "block";
+    prompt.style.display = "none";
+    reset_stuff.style.display = "none";
+  }
+
+  else if (widget_1_widget === "proxy") {
+    widget_btns.style.display = "none";
+    proxy_options.style.display = "block";
+    prompt.style.display = "none";
+    reset_stuff.style.display = "none";
+  }
+
+  else if (widget_1_widget === "unblocker") {
+    widget_btns.style.display = "none";
+    unblocker_options.style.display = "block";
     prompt.style.display = "none";
     reset_stuff.style.display = "none";
   }
@@ -141,6 +157,16 @@ const return_imperium = document.getElementById("return-imp");
 const finder = document.getElementById("finder");
 const return_finder = document.getElementById("return-af");
 
+const proxy = document.getElementById("proxy");
+const return_proxy = document.getElementById("return-proxy");
+const proxy_form = document.getElementById("proxy-form");
+const proxy_url = document.getElementById("proxy-url");
+
+const unblocker = document.getElementById("unblocker");
+const return_unblocker = document.getElementById("return-unblocker");
+const unblocker_form = document.getElementById("unblocker-form");
+const unblocker_url = document.getElementById("unblocker-url");
+
 imperium.onclick = function () {
   if (selected_widget === 1) {
     widget_1_widget = "imperium";
@@ -198,6 +224,76 @@ finder.onclick = function () {
 return_finder.onclick = function () {
   widget_btns.style.display = "block";
   af_options.style.display = "none";
+  prompt.style.display = "block";
+  reset_stuff.style.display = "block";
+}
+
+proxy.onclick = function () {
+  if (selected_widget === 1) {
+    widget_1_widget = "proxy";
+    widget1_btn.style.fontSize = "20px";
+    widget1_btn.innerText = "Proxy";
+  }
+
+  else if (selected_widget === 2) {
+    widget_2_widget = "proxy";
+    widget2_btn.style.fontSize = "20px";
+    widget2_btn.innerText = "Proxy";
+  }
+
+  else {
+    widget_3_widget = "proxy";
+    widget3_btn.style.fontSize = "20px";
+    widget3_btn.innerText = "Proxy";
+  }
+
+  return_btn.click();
+}
+
+proxy_form.onsubmit = function () {
+  event.preventDefault();
+  window.open("https://translate.google.com/translate?sl=auto&tl=en&u=" + proxy_url.value);
+  proxy_url.value = "";
+}
+
+return_proxy.onclick = function () {
+  widget_btns.style.display = "block";
+  proxy_options.style.display = "none";
+  prompt.style.display = "block";
+  reset_stuff.style.display = "block";
+}
+
+unblocker.onclick = function () {
+  if (selected_widget === 1) {
+    widget_1_widget = "unblocker";
+    widget1_btn.style.fontSize = "20px";
+    widget1_btn.innerText = "Unblocker";
+  }
+
+  else if (selected_widget === 2) {
+    widget_2_widget = "unblocker";
+    widget2_btn.style.fontSize = "20px";
+    widget2_btn.innerText = "Unblocker";
+  }
+
+  else {
+    widget_3_widget = "unblocker";
+    widget3_btn.style.fontSize = "20px";
+    widget3_btn.innerText = "Unblocker";
+  }
+
+  return_btn.click();
+}
+
+unblocker_form.onsubmit = function () {
+  event.preventDefault();
+  window.open("https://borgcube.codesalvageon.repl.co/u/" + unblocker_url.value);
+  unblocker_url.value = "";
+}
+
+return_unblocker.onclick = function () {
+  widget_btns.style.display = "block";
+  unblocker_options.style.display = "none";
   prompt.style.display = "block";
   reset_stuff.style.display = "block";
 }
